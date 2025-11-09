bb0a93a1568a6eba34b1743b2921c75b36d8f7cf
nikinarine2-arch <nikinarine2@outlook.com>
2025-11-09 09:32:38 +0200

    fix: Revert module to application to fix CI build

--- game_match_letter/build.gradle.kts
+++ game_match_letter/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library")
+    id("com.android.application")
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("kotlin-kapt")
@@ -12,8 +12,11 @@ android {
     }
 
     defaultConfig {
+        applicationId = "com.example.brightbuds"
         minSdk = 21
         targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
